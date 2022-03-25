d1821186cd384260eabe733cdafa48dffa9cc406
Steven Garcia <sjosegarcia23@gmail.com>
2022-03-25 17:40:36 -0400

    updated tests, made it clearer.

--- test/testTokenFarm.ts
+++ test/testTokenFarm.ts
@@ -2,15 +2,15 @@ import { assert, expect } from 'chai';
 import { ethers } from 'hardhat';
 import { CGToken, DaiToken, TokenFarm } from '../typechain';
 
-describe('TokenFarm', async () => {
+describe('TokenFarm Contract', async () => {
 	let cgToken: CGToken;
 	let mDaiToken: DaiToken;
 	let tokenFarm: TokenFarm;
+	const accounts = await ethers.getSigners();
 
 	before(async () => {
 		const cgTokenContract = await ethers.getContractFactory('CGToken');
 		cgToken = await cgTokenContract.deploy();
-		await cgToken.deployed();
 		const mDaiTokenContract = await ethers.getContractFactory('DaiToken');
 		mDaiToken = await mDaiTokenContract.deploy();
 		await mDaiToken.deployed();
@@ -19,28 +19,37 @@ describe('TokenFarm', async () => {
 			cgToken.address,
 			mDaiToken.address
 		);
-		await tokenFarm.deployed();
 	});
-	describe('TokenFarm', () => {
-		it('Check if CG token contract has deployed', async () => {
+	describe('CG Token Deployment', async () => {
+		it('Deploy CG Token', async () => {
+			await cgToken.deployed();
 			assert(cgToken.name, 'CG Token');
 		});
-		it('Check if Dai token contract has deployed', async () => {
+	});
+	describe('mDai Token Deployment', async () => {
+		it('Deploy mDai Token', async () => {
+			await mDaiToken.deployed();
 			assert(mDaiToken.name, 'Mock Dai Token');
 		});
+	});
+	describe('TokenFarm Deployment', () => {
 		it('Check if TokenFarm contract has deployed', async () => {
+			await tokenFarm.deployed();
 			assert(tokenFarm.name, 'CG Token Farm');
 		});
+	});
+	describe('TokenFarm Transfer', async () => {
 		it('Funding TokenFarm Contract', async () => {
 			await cgToken.transfer(tokenFarm.address, '1000000000000000000');
 			const tokenFarmBalance = await cgToken.balanceOf(tokenFarm.address);
 			expect(tokenFarmBalance.gt(0));
 		});
+	});
+	describe('Fund mDAI to EOA', async () => {
 		it('Funding EOA with mDAI', async () => {
-			const accounts = await ethers.getSigners();
 			await mDaiToken.transfer(accounts[0].address, '1000000000000000000');
-			const eoaDaiBalance = await mDaiToken.balanceOf(accounts[0].address);
-			expect(eoaDaiBalance.gt(0));
+			const mDaiBalance = await mDaiToken.balanceOf(accounts[0].address);
+			expect(mDaiBalance.gt(0));
 		});
 	});
 });
