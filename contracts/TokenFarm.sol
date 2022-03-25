17384f1d31af71f2e8ce9c7d7d1fd85cb96c2b4d
Steven Garcia <sjosegarcia23@gmail.com>
2022-03-25 00:09:33 -0400

    added code to deploy contracts and send balance

--- contracts/TokenFarm.sol
+++ contracts/TokenFarm.sol
@@ -1,6 +1,15 @@
 pragma solidity ^0.8.10;
+import "./CGToken.sol";
+import "./DaiToken.sol";
 
 contract TokenFarm {
 
     string public name = "CG Token Farm";
+    CGToken public cgToken;
+    DaiToken public daiToken;
+
+    constructor(CGToken _cgToken, DaiToken _daiToken) {
+        cgToken = _cgToken;
+        daiToken = _daiToken;
+    }
 }
--- scripts/tokenAndFarm.ts
+++ scripts/tokenAndFarm.ts
@@ -1,12 +1,28 @@
 import { ethers } from 'hardhat';
 
 const tokeknFarmDeployer = async () => {
+	const cgTokenContract = await ethers.getContractFactory('CGToken');
+	const daiTokenContract = await ethers.getContractFactory('DaiToken');
+	const cgToken = await cgTokenContract.deploy();
+	const daiToken = await daiTokenContract.deploy();
+	await cgToken.deployed();
+	await daiToken.deployed();
 	const tokenFarmContract = await ethers.getContractFactory('TokenFarm');
-	const tokeFarm = await tokenFarmContract.deploy();
+	const tokenFarm = await tokenFarmContract.deploy(
+		cgToken.address,
+		daiToken.address
+	);
 
-	await tokeFarm.deployed();
+	await tokenFarm.deployed();
 
-	console.log('Token Farm deployed to:', tokeFarm.address);
+	console.log('Token Farm deployed to:', tokenFarm.address);
+	await cgToken.transfer(tokenFarm.address, '1000000000000000000');
+	const tokenFarmBalance = await cgToken.balanceOf(tokenFarm.address);
+	console.log('TokenFarm balance of CG Token: ', tokenFarmBalance);
+	const accounts = await ethers.getSigners();
+	await daiToken.transfer(accounts[0].address, '1000000000000000000');
+	const eoaDaiBalance = await daiToken.balanceOf(accounts[0].address);
+	console.log('Dai balance of account', eoaDaiBalance);
 };
 
 tokeknFarmDeployer().catch((error) => {
