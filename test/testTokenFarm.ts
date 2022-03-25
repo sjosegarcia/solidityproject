439900b70d8339b69196b3f990332cad8d25c7bf
Steven Garcia <sjosegarcia23@gmail.com>
2022-03-25 10:52:09 -0400

    added more assertion tests

--- test/testTokenFarm.ts
+++ test/testTokenFarm.ts
@@ -1,10 +1,12 @@
 import { assert, expect } from 'chai';
 import { ethers } from 'hardhat';
-import { CGToken, DaiToken } from '../typechain';
+import { CGToken, DaiToken, TokenFarm } from '../typechain';
 
 describe('TokenFarm', async () => {
 	let cgToken: CGToken;
 	let mDaiToken: DaiToken;
+	let tokenFarm: TokenFarm;
+
 	before(async () => {
 		const cgTokenContract = await ethers.getContractFactory('CGToken');
 		cgToken = await cgTokenContract.deploy();
@@ -12,6 +14,12 @@ describe('TokenFarm', async () => {
 		const mDaiTokenContract = await ethers.getContractFactory('DaiToken');
 		mDaiToken = await mDaiTokenContract.deploy();
 		await mDaiToken.deployed();
+		const tokenFarmContract = await ethers.getContractFactory('TokenFarm');
+		tokenFarm = await tokenFarmContract.deploy(
+			cgToken.address,
+			mDaiToken.address
+		);
+		await tokenFarm.deployed();
 	});
 	describe('TokenFarm', () => {
 		it('Check if CG token contract has deployed', async () => {
@@ -19,6 +27,9 @@ describe('TokenFarm', async () => {
 		});
 		it('Check if Dai token contract has deployed', async () => {
 			assert(mDaiToken.name, 'Mock Dai Token');
+		});
+		it('Check if TokenFarm contract has deployed', async () => {
+			assert(tokenFarm.name, 'CG Token Farm');
 		});
 	});
 });
