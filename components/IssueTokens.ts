786d416b0ff6e9242edc64570c91390e00365a53
Steven Garcia <sjosegarcia23@gmail.com>
2022-04-01 11:33:49 -0400

    called the contract properly through hardhat

--- components/IssueTokens.ts
+++ components/IssueTokens.ts
@@ -1,9 +1,9 @@
 import { ethers } from 'hardhat';
-import { abi as tokenFarmAbi } from '../artifacts/contracts/TokenFarm.sol/TokenFarm.json';
 
 const issueToken = async () => {
-	const tokenFarm = new ethers.Contract(
-		'0x9fE46736679d2D9a65F0992F2272dE9f3c7fa6e0',
-		tokenFarmAbi
+	const tokenFarmContract = ethers.getContractFactory('TokenFarm');
+	const tokenFarm = (await tokenFarmContract).attach(
+		'0x9fE46736679d2D9a65F0992F2272dE9f3c7fa6e0'
 	);
+	await tokenFarm.issueTokens();
 };
