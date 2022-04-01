8d944973b952ed8252b7b6873f8654db6512bd7a
Steven Garcia <sjosegarcia23@gmail.com>
2022-04-01 11:39:34 -0400

    contractAt

--- components/IssueTokens.ts
+++ components/IssueTokens.ts
@@ -1,7 +1,6 @@
 import { ethers } from 'hardhat';
 import { tokenFarm as tokenFarmAddress } from '../contracts.json';
 const issueToken = async () => {
-	const tokenFarmContract = await ethers.getContractFactory('TokenFarm');
-	const tokenFarm = tokenFarmContract.attach(tokenFarmAddress);
+	const tokenFarm = await ethers.getContractAt('TokenFarm', tokenFarmAddress);
 	await tokenFarm.issueTokens();
 };
