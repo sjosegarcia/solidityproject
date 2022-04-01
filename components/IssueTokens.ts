28be8ea4cb2667380b616f2e9acd4fc1fab25755
Steven Garcia <sjosegarcia23@gmail.com>
2022-04-01 11:38:14 -0400

    added a json file to add contract addresses

--- components/IssueTokens.ts
+++ components/IssueTokens.ts
@@ -1,9 +1,7 @@
 import { ethers } from 'hardhat';
-
+import { tokenFarm as tokenFarmAddress } from '../contracts.json';
 const issueToken = async () => {
-	const tokenFarmContract = ethers.getContractFactory('TokenFarm');
-	const tokenFarm = (await tokenFarmContract).attach(
-		'0x9fE46736679d2D9a65F0992F2272dE9f3c7fa6e0'
-	);
+	const tokenFarmContract = await ethers.getContractFactory('TokenFarm');
+	const tokenFarm = tokenFarmContract.attach(tokenFarmAddress);
 	await tokenFarm.issueTokens();
 };
